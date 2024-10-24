dbb5607978f4ca9685e4bbebc5def180944028db
MYousuf-Codes <imhere4uinfo@gmail.com>
2024-10-25 00:51:55 +0500

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 
 import Home from "./home/page";
-import About from "./about/page";
-import Contact from "./contact/page";
+// import About from "./about/page";
+// import Contact from "./contact/page";
 
 export default function hero() {
   return (
    <main>
     <Home/>
-    <About/>
-    <Contact/>
+    {/* <About/>
+    <Contact/> */}
    </main>
   );
 }
